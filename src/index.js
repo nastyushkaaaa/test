0d4cd51f527849a78d4eadf2b5a2f43f86467588
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-10-07 22:44:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/test">
+      <BrowserRouter basename="/test/">
         <App />
       </BrowserRouter>
     </Provider>
